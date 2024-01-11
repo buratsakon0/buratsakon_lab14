75545c271c0b6269b6679413cf10fb4e89e6b759
buratsakon0 <buratsakon_s@cmu.ac.th>
2024-01-11 14:07:20 +0700

    lab14_2.cpp completed

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -19,3 +19,44 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double a[][N])
+{
+	for (int i = 0; i < N; i++)
+	{
+		cout << "Row " << i + 1 << ": ";
+		for (int j = 0; j < N; j++)
+		{
+			cin >> a[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double A[][N], bool B[][N])
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			B[i][j] = (bool)0;
+			if (i != 0 && j != 0 && i != N - 1 && j != N - 1)
+			{
+				double x = A[i][j];
+				if (x >= A[i][j] && x >= A[i + 1][j] && x >= A[i][j + 1] && x >= A[i - 1][j] && x >= A[i][j - 1])
+					B[i][j] = (bool)1;
+			}
+		}
+	}
+}
+
+void showMatrix(const bool A[][N])
+{
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			cout << (A[i][j]) << " ";
+		}
+		cout << endl;
+	}
+}
